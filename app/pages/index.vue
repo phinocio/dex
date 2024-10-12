7f74e4b27173b227b93a3d215ed9f9e5ed4e5be7
Phinocio <github@phinocio.com>
2024-10-11 23:38:35 -0600

    feat: show available dexes on home page; show updates

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,26 +6,52 @@ useSeoMeta({
 });
 
 const {
-	data: dex,
+	data: dexes,
 	status,
 	error,
-} = await useLazyFetch<{ data: ApiGameDex }>(`${config.public.apiBase}/game-dexes/rby-national?include=pokemon`);
+} = await useLazyFetch<{ data: ApiGameDex[] }>(`${config.public.apiBase}/game-dexes`);
 </script>
 
 <template>
-	<div>
-		<header class="mb-12 flex items-center justify-between">
-			<h1 class="text-3xl font-bold">Your Dexes <small class="font-normal">(0)</small></h1>
-			<button
-				class="hover:shadow-dark flex items-center rounded-xl border border-pink-500 px-4 py-2 font-bold text-pink-500 hover:bg-pink-500 hover:text-white"
-				@click="$router.push('/dexes/create')"
-			>
-				New Dex <Icon name="ph:pencil" :size="24" class="ml-2 inline" />
-			</button>
-		</header>
-
-		<p v-if="status === 'pending'">Loading...</p>
-		<p v-for="pokemon in dex?.data.pokemon" v-else :key="pokemon.id">{{ pokemon.name }}</p>
-		<div v-if="error">{{ error }}</div>
+	<div class="grid gap-10 md:grid-cols-2 xl:grid-cols-4">
+		<article
+			class="border-border-light dark:border-border-dark rounded-xl border bg-[#dce0e8] p-4 shadow-xl xl:col-span-3 dark:bg-[#11111b]"
+		>
+			<header class="mb-4 flex items-center justify-between">
+				<h2 class="text-3xl font-bold">Available Dexes</h2>
+			</header>
+			<div class="grid gap-10 xl:grid-cols-3">
+				<p v-if="status === 'pending'">Loading...</p>
+				<section
+					v-for="dex in dexes?.data"
+					v-else
+					:key="dex.slug"
+					class="bg-light dark:bg-dark rounded-xl border border-blue-500"
+				>
+					<header class="flex items-center justify-between border-b border-blue-500">
+						<div class="flex rounded-xl px-3 py-2 font-bold">
+							<Icon name="ph:notebook" :size="24" class="mr-2 inline" />
+							<span>{{ dex.name }}</span>
+						</div>
+						<button class="flex space-x-2 px-3 py-2 font-bold text-blue-500">
+							<Icon name="ph:plus-bold" :size="24" />
+							<span>Create</span>
+						</button>
+					</header>
+					<div class="p-4">
+						<p v-for="game in dex.games" :key="game.slug">{{ game.name }}</p>
+					</div>
+				</section>
+				<div v-if="error">{{ error }}</div>
+			</div>
+		</article>
+		<article
+			class="border-border-light dark:border-border-dark rounded-xl border bg-[#dce0e8] p-4 shadow-xl dark:bg-[#11111b]"
+		>
+			<header class="mb-4 flex items-center justify-between">
+				<h2 class="text-3xl font-bold">Updates</h2>
+			</header>
+			Gen 1-9 Support added!
+		</article>
 	</div>
 </template>
